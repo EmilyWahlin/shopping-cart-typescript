e73c3fc4f343768904de2323534d233dfcf1bf32
EmilyWahlin <emilysaletti@gmail.com>
2025-07-11 17:50:52 +0200

    added functionality to button

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -10,10 +10,15 @@ type CartItem = {
 }
 
 type ShoppingCartContext = {
+  openCart: () => void
+  closeCart: () => void
+
   getItemQuantity: (id: number) => number
   increaseCartQuantity: (id: number) => void
   decreaseCartQuantity: (id: number) => void
   removeFromCart: (id: number) => void
+  cartQuantity: number
+  cartItems: CartItem[]
 }
 
 const ShoppingCartContext = createContext({})
@@ -26,7 +31,17 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }
   : ShoppingCartProviderProps) {
+  const [isOpen, setIsOpen] = useState(false)
   const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  )
+
+  const openCart = () => setIsOpen(true)
+  const closeCart = () => setIsOpen(false)
+
 
   function getItemQuantity(id: number) {
     return cartItems.find(item => item.id === id)?.quantity || 0
@@ -64,10 +79,14 @@ export function ShoppingCartProvider({ children }
   })
   }
 
-  
+  function removeFromCart(id: number) {
+    setCartItems(currItems => {
+      return currItems.filter(item => item.id !== id)
+    })
+  }
 
   return (
-    <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity}}>
+    <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart,openCart, closeCart,cartQuantity}}>
     {children}
     </ShoppingCartContext.Provider>
   )
